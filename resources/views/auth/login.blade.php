123f7d42112619e9a3ae3f2c8f96f08a71636941
Aboajah Emmanuel <aboajah.emmanuel@fmdqgroup.com>
2025-10-21 09:02:40 +0100

    test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
                 <img src="{{ asset('public/users/assets/FMDQ-Logo.png') }}" alt="FMDQlogo" />
             </div>
             <div class="app-name">
-                <p>FINANCIAL MARKETS RULES REPOSITORY</p>
+                <p>FINANCIAL MARKETS ccccccccccccccccccccccccRULES REPOSITORY</p>
             </div>
             <div class="tabs">
                 <div class="current">
