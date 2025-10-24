28e4553d07cadee00e6a1871801e5425f732b2a1
Aboajah Emmanuel <aboajah.emmanuel@fmdqgroup.com>
2025-10-24 11:24:48 +0100

    Document Mapping

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
                 <img src="{{ asset('public/users/assets/FMDQ-Logo.png') }}" alt="FMDQlogo" />
             </div>
             <div class="app-name">
-                <p>FINANCIAL MARKETS RULES REPOSITOR<p>lflflfl
+                <p>FINANCIAL MARKETS RULES REPOSITOR<p>
             </div>
             <div class="tabs">
                 <div class="current">
