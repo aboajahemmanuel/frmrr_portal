3b6eb7da6ba53d72dfb7bf4809daf12f87722ef2
Aboajah Emmanuel <aboajah.emmanuel@fmdqgroup.com>
2025-11-01 08:33:47 +0100

    deploy

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
                 <img src="{{ asset('public/users/assets/FMDQ-Logo.png') }}" alt="FMDQlogo" />
             </div>
             <div class="app-name">
-                <p>FINANCIA<p>
+                <p> MARKETS<p>
             </div>
             <div class="tabs">
                 <div class="current">
