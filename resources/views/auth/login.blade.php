9628dcdcee9afdc8f4705a0266cb45ebd4a3c0d7
Aboajah Emmanuel <aboajah.emmanuel@fmdqgroup.com>
2025-11-03 12:49:58 +0100

    Payment Update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
                 <img src="{{ asset('public/users/assets/FMDQ-Logo.png') }}" alt="FMDQlogo" />
             </div>
             <div class="app-name">
-                <p> PEOPLE S S<p>
+                <p>FINANCIAL MARKETS RLES REPOSITOR<p>
             </div>
             <div class="tabs">
                 <div class="current">
