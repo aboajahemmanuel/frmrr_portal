df0638e84aaf1193c54c451a4f104a9e0b6867d6
Aboajah Emmanuel <aboajah.emmanuel@fmdqgroup.com>
2026-07-10 09:17:40 +0100

    payment update

--- payreturn.php
+++ payreturn.php
@@ -4,7 +4,7 @@ echo  $status =  $_POST["newStatus"];
 
 
 
-header("Location: https://adgtest.fmdqgroup.com/deploy_fmrr/payment/success?status=$status");
+header("Location: http://101-php-01.fmdqgroup.com/deploy_fmrr/payment/success?status=$status");
 //header("Location: http://localhost/fmrr_dev/payment/success?status=$status");
 
 
